7d1525be453d5a19590b080485415be785bf17f4
Harsh Sahu <harshsahu0702@gmail.com>
2025-12-30 11:16:10 +0530

    added the logo

--- hostel-hub/src/components/Header.jsx
+++ hostel-hub/src/components/Header.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import "./Header.css";
 import TaglineCTA from "./TaglineCTA";
+import logo from "../assets/logo.png";
+
 export default function Header() {
-   
+
   return (
     <header className="header-component">
       <div className="header-content">
-        <div className="logo-box large">HC</div>
+        <div className="logo-box large">
+          <img src={logo} alt="Logo" className="w-full h-full object-cover rounded-full" />
+        </div>
         <div className="title-section large">
           <h1 className="site-title">Hostel-<span className="hub-accent">Hub</span></h1>
-        
+
         </div>
-        
+
       </div>
-      <TaglineCTA />  
+      <TaglineCTA />
     </header>
   );
 }
